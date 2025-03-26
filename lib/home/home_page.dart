82e1773f158bc18f2b848baef48ced1357eb7c38
Leonardo Tacca <leotacca@hotmail.com>
2025-03-26 08:43:41 -0300

    Criação do menu e logica de navegação lateral

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  PageController controllerTelas = PageController();
+  SideMenuController sideMenu = SideMenuController();
+  @override
+  void initState() {
+    sideMenu.addListener((index) {
+      controllerTelas.jumpToPage(index);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List items = [
+      SideMenuItem(
+        title: 'Dashboard',
+        onTap: (index, sideMenuController) {
+          sideMenuController.changePage(index);
+        },
+        icon: const Icon(Icons.home),
+      ),
+      SideMenuItem(
+        title: 'Gerenciamento de usuarios',
+        onTap: (index, sideMenuController) {
+          sideMenuController.changePage(index);
+        },
+        icon: const Icon(Icons.settings),
+      ),
+      SideMenuItem(
+        title: 'Gerenciamento de maquinas',
+        onTap: (index, sideMenuController) {
+          sideMenuController.changePage(index);
+        },
+        icon: const Icon(Icons.settings),
+      ),
+    ];
+    return Scaffold(
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.red[200],
+            child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              SideMenu(
+                controller: sideMenu,
+                style: SideMenuStyle(backgroundColor: Colors.white),
+                onDisplayModeChanged: (mode) {
+                  print(mode);
+                },
+                items: items,
+              ),
+              Expanded(
+                child: PageView(
+                  controller: controllerTelas,
+                  children: const [
+                    Center(
+                      child: Text('Bem vindo ao gerenciador de manutenção'),
+                    ),
+                    Center(
+                      child: Text('Exit'),
+                    ),
+                    Center(
+                      child: Text('Exit'),
+                    ),
+                  ],
+                ),
+              ),
+            ])));
   }
 }
--- lib/login/auth_service.dart
+++ lib/login/auth_service.dart
@@ -5,17 +5,18 @@ import 'package:flutter/material.dart';
 
 class AuthService extends ChangeNotifier {
   Future<bool> login(String email, String senha) async {
-    final response = await http.post(
-      Uri.parse('localhost:3000/login'),
-      headers: {"Content-Type": "application/json"},
-      body: jsonEncode({"email": email, "password": senha}),
-    );
+    // final response = await http.post(
+    //   Uri.parse('localhost:3000/login'),
+    //   headers: {"Content-Type": "application/json"},
+    //   body: jsonEncode({"email": email, "password": senha}),
+    // );
 
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      return true;
-    } else {
-      return false;
-    }
+    // if (response.statusCode == 200) {
+    //   final data = jsonDecode(response.body);
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    return true;
   }
 }
